e1ec6a37eb5d42d7d35337010758abe5126a1491
Gabriel Perez <gperez@iastate.edu>
2024-08-07 00:41:09 -0500

    feat: Add functional Jetty server to web application

--- ComSciVisualized/src/main/java/com/comscivisualized/backend/Main.java
+++ ComSciVisualized/src/main/java/com/comscivisualized/backend/Main.java
@@ -3,14 +3,21 @@ package com.comscivisualized.backend;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
-        WebAppContext context = new WebAppContext();
-        context.setContextPath("/");
-        context.setResourceBase("src/main/frontend");
-        context.setParentLoaderPriority(true);
-        server.setHandler(context);
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setContextPath("/");
+        webAppContext.setResourceBase("src/main/frontend");
+        webAppContext.setParentLoaderPriority(true);
+
+        // Enable directory listings if there's no index.html
+        webAppContext.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "true");
+
+        server.setHandler(webAppContext);
+
         server.start();
         server.join();
     }
